d2be6e361de3954815c5a6fa3c82566bf75d4977
Jose Castro <drakonc@users.noreply.github.com>
2019-05-16 14:03:57 -0500

    Agregar archivos de proyecto.

---
+++ ServerPing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace ServerPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> ips = new List<string>();
+            var ips = ConfigurationSettings.AppSettings["lips"];
+            List<string> Lista_Ip = ips.Split('|').ToList();
+
+            //Lista_Ip.ForEach(a =>
+            //{
+            //    Console.WriteLine(a);
+            //});
+            //Console.ReadLine();
+
+            clPing p = new clPing();
+            p.EscribirArchivo(Lista_Ip);
+
+        }
+    }
+}
---
+++ ServerPing/clPing.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace ServerPing
+{
+    public class clPing
+    {
+        public void EscribirArchivo(List<string> ips)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string name = "ping_" + DateTime.Now.Day.ToString() + "_" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Year.ToString() + ".txt";
+            string txt = path + @"\" + name;
+            string salto = "\n";
+            string header = "";
+
+            using (StreamWriter escribe = new StreamWriter(txt))
+            {
+                for (int i = 0; i < ips.Count; i++)
+                {
+                    header = "<--------------PING: " + ips[i] + "--------------><br>";
+                    List<string> respuesta = SimplePing(ips[i]);
+                    escribe.WriteLine(header);
+
+                    if (respuesta != null)
+                    {
+                        for (int j = 0; j < respuesta.Count; j++)
+                        {
+                            escribe.Write(respuesta[j].ToString());
+                        }
+                    }
+                    else
+                    {
+                        escribe.Write("La IP: " + ips[i] + "Es Inalcansable<br>");
+                    }
+
+                    escribe.WriteLine(salto);
+                }                
+
+                escribe.WriteLine(salto);
+            }
+
+        }
+
+
+        public List<string> SimplePing(string ip)
+        {
+            try
+            {
+                Ping pingSender = new Ping();
+                PingReply reply;
+                string cadena = "";
+                List<string> respuesta = new List<string>();
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        reply = pingSender.Send(ip);
+                        cadena += "IP: " + reply.Address.ToString() + " bytes: " + reply.Buffer.Length.ToString() + " tiempo: " + reply.RoundtripTime.ToString() + "ms TTL: " + reply.Options.Ttl.ToString() + "<br>" + Environment.NewLine;
+                        respuesta.Add(cadena);
+                        cadena = "";
+                    }
+
+                return respuesta;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
